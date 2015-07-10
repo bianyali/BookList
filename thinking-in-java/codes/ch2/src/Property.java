5e417f32f28b21e0b82f2465ef0ca2912d6dc8d8
Yali Bian <byl.lisp@gmail.com>
2015-07-10 15:30:13 +0800

    add comments and Documentation

--- thinking-in-java/codes/ch2/src/Property.java
+++ thinking-in-java/codes/ch2/src/Property.java
@@ -1,12 +1,21 @@
-/**
- * Created by Yali Bian on 7/9/15.
- */
-
+//: Property.java
 
 import java.util.*;
 
+
+
+/** The first Thinking in Java example program.
+ * Lists system information on current machine.
+ * @author Yali Bian
+ * @author http://lisp.one
+ * @version 1.0
+ */
 public class Property {
 
+    /** Sole entry point to class & application
+     * @param args array of string arguemts
+     * @return No return value
+     * */
     public static void main (String [] args){
         System.out.println(new Date());
         Properties p = System.getProperties();
@@ -19,4 +28,4 @@ public class Property {
                 + " Free memory = "
                 + rt.freeMemory());
     }
-}
+} ///:~
