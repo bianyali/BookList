8759fbe321008fdc0e2303f499d24e845dfc9114
Yali Bian <byl.lisp@gmail.com>
2015-08-06 20:45:43 +0800

    add yin-yang problem in 'The Scheme Programming Language'

---
+++ tspl/ibm-codes/yin-yang.rkt
@@ -0,0 +1,17 @@
+#lang racket
+
+
+(let* ((yin ((lambda (foo)
+               (newline)
+               foo)
+             (call/cc (lambda (bar)
+                        bar))))
+
+       (yang ((lambda (foo)
+                (write-char #\*)
+                foo)
+              (call/cc
+               (lambda (bar)
+                 bar)))))
+
+  (yin yang))
